53679496a00f9544fdad5605d2b2d625786e1022
Iker Nadal <iker1359@gmail.com>
2021-11-18 00:46:05 +0100

    Subo lo hecho en casa de la practica 2 de hipotenochas

--- Practica2Hipotenochas/app/src/main/java/com/example/practica2_hipotenochas/MainActivity.java
+++ Practica2Hipotenochas/app/src/main/java/com/example/practica2_hipotenochas/MainActivity.java
@@ -2,10 +2,15 @@ package com.example.practica2_hipotenochas;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.Image;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TableLayout;
 import android.widget.TableRow;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,36 +22,31 @@ public class MainActivity extends AppCompatActivity {
         init();
     }
 
+    //Este metodo solo está hecho para la dificultad facil, luego hay que repetirlo para la media y dificil
     public void init() {
 
-        int rowsColsEasy = 8;
-        int rowsColsMedium = 12;
-        int rowsColsHard = 16;
+        ArrayList<String> listaHipotenochados = new ArrayList<String>();
 
+        int rowsColsEasy = 8;
         int generadorID = 1;
 
-        int hipotenochasEasy = 10;
-        int hipotenochasMedium = 20;
-        int hipotenochasHard = 30;
-
         TableLayout tl = (TableLayout) findViewById(R.id.TableLayout1);
-
-        //https://stackoverflow.com/questions/12833430/layout-parameters-for-tablerow-views
-        //https://stackoverflow.com/questions/18207470/adding-table-rows-dynamically-in-android
-        //links para layout parameters
 
         for (int filas = 1; filas <= rowsColsEasy; filas ++, generadorID ++) {
 
             //En esta primera parte hago un generador de tags y creo la fila
             String rowNameGenerator = ("row" + (String.valueOf(filas)));
 
-            TableRow.LayoutParams tlParams = new TableRow.LayoutParams(
-                    TableRow.LayoutParams.MATCH_PARENT,
-                    TableRow.LayoutParams.MATCH_PARENT);
+            TableRow row = new TableRow(this);
 
-            TableRow rowCreada = new TableRow(this);
-            rowCreada.setId(generadorID);
-            rowCreada.setTag(rowNameGenerator);
+            TableRow.LayoutParams trParams = new TableRow.LayoutParams(
+                    TableRow.LayoutParams.MATCH_PARENT,
+                    TableRow.LayoutParams.MATCH_PARENT,
+                    1);
+
+            row.setId(generadorID);
+            row.setTag(rowNameGenerator);
+            row.setLayoutParams(trParams);
 
             for (int columnas = 1; columnas <=rowsColsEasy; columnas ++, generadorID ++) {
 
@@ -54,18 +54,38 @@ public class MainActivity extends AppCompatActivity {
                 //Creo el boton y lo añado a la fila
                 String columnNameGenerator = (rowNameGenerator + "col" + (String.valueOf(columnas)));
 
-                Button boton = new Button(this);
+                ImageButton boton = new ImageButton(this);
+
                 boton.setId(generadorID);
                 boton.setTag(columnNameGenerator);
-                rowCreada.addView(boton);
+
+                boton.setImageResource(R.drawable.elwafels);
+                boton.setScaleType(ImageView.ScaleType.FIT_XY);
+                boton.setAdjustViewBounds(true);
+
+                boton.setLayoutParams(trParams);
+
+                row.addView(boton);
 
             }
 
             //Añado la fila entera con las columnas incluidas al tableLayout
 
-            tl.addView(rowCreada);
+            tl.addView(row);
 
-        }
+        } //Fin del for para crear el table layout
+
+        //Aqui va a empezar lo de poner hipotenochas
+        /*
+        *
+        * La idea es que tengo un contador de hipotenochas(las que tienen que salir) y con un Math.random() voy generando un numero entre 1 y el nº total de casillas.
+        * Después se busca esa casilla (por ID o Tag) se indica que se ha puesto las hipotenocha y esa casilla se va a añadir al arrayList para que asi si salen casillas repetidas
+        * se haga otro numero aleatorio, para que no haya duplicados.
+        *
+        * */
+
+        int hipotenochasEasy = 10;
+
 
     }
 }
